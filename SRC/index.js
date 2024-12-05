86b6f4da8a20f95211de73bc6e2fdb0453b406e1
unknown <selinaadwoa1@gmail.com>
2024-12-05 14:39:58 -0800

    adding changes

--- SRC/index.js
+++ SRC/index.js
@@ -14,3 +14,29 @@ let sel = document.getElementById("sel");
 sel.style.backgroundColor = "Yellow";
 sel.style.color = "black";
 sel.innerHTML = "I know how to access/select elements in the DOM 👊";
+
+
+
+
+//Using Js to style my third paragraph
+let use = document.getElementById("use");
+use.style.backgroundColor = "green";
+use.style.color = "black";
+use.innerHTML = "I know how to use the style methods to change the style of an element in the DOM ✅";
+
+
+//Using Js to style my fourth paragraph
+let styl = document.getElementById("styl");
+styl.style.backgroundColor = "purple";
+styl.style.color = "white";
+styl.innerHTML = "I know how the styling properties differ in JS from CSS👍";
+
+
+//using Js to style my fifth paragraph
+let excited = document.getElementById("Excited");
+excited.style.backgroundColor = "black";
+excited.style.color = "white"
+excited.innerText = "Yes I did it!! I manipulated the DOM and styled my elements 🥰🥰🥰🥰"
+
+
+
