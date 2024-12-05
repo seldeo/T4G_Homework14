b3173cc1b8da5d48599b5ee5e6a8dd574988c201
unknown <selinaadwoa1@gmail.com>
2024-12-05 13:58:44 -0800

    adding SRC folder and index.html

---
+++ SRC/index.js
@@ -0,0 +1,16 @@
+let title = document.getElementById("title");
+title.style.backgroundColor = "blue";
+
+
+//Using Js to style my first paragraph
+let DOM = document.getElementById("DOM");
+DOM.style.backgroundColor = "gray";
+DOM.style.color = "black"
+DOM.innerHTML = "I know now what the DOM is 🤲 "
+
+
+//Using Js to style my Seconf Paragraph
+let sel = document.getElementById("sel");
+sel.style.backgroundColor = "Yellow";
+sel.style.color = "black";
+sel.innerHTML = "I know how to access/select elements in the DOM 👊";
